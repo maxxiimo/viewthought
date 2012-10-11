b96e63db7b89f20168428580e2de5f1f97379012
Chris Maxwell <cmaxwell@ojala.com>
2012-10-11 14:30:11 -0400

    Set up foundation styles.

---
+++ config/compass.rb
@@ -0,0 +1,9 @@
+# Require any additional compass plugins here.
+project_type = :rails
+
+# To allow compass to import partials from subdirectories per:
+# http://blog.55minutes.com/2012/01/getting-compass-to-work-with-rails-31-and-32/
+# additional_import_paths = ["app/assets/stylesheets/<name>", "app/assets/stylesheets/<name>"]
+#
+# To compile in the necessary debugging information for FireSass.
+sass_options = {:debug_info => true}
