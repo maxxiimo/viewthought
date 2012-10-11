2a37aeadc158357b3e21f6960fbb9c3eabc338ab
Chris Maxwell <cmaxwell@ojala.com>
2012-10-11 12:36:08 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Viewthought::Application.config.secret_token = '01eee4e47fcf3173f07ae34073b6f5abcfc5b44dcc4566e75c97c6251fbb75c7809f89052aa8ddacf4427c24b68e848ab32a083a746cc1c26042eaedb2eaf079'
