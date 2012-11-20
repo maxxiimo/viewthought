b6649980e3ead284ca832ead5f4198baf2a9b388
Chris Maxwell <cmaxwell@ojala.com>
2012-11-20 11:48:08 -0500

    Revert Mobtlette and jQuery mobile implementation.

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.8'
 
-gem 'mobylette'
 gem 'jquery-rails', '2.0.2'
 gem 'haml-rails'
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  include Mobylette::RespondToMobileRequests
-  mobylette_config do |config|
-    config[:skip_xhr_requests] = false
-  end
 end
