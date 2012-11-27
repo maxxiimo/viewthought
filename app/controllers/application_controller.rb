8af9f485a371d2cfda8e2b4ae4f901b4b4810307
Chris Maxwell <cmaxwell@ojala.com>
2012-11-27 17:34:23 -0500

    Configure application for mobile via Ryan Bates and Mobvious.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include Mobvious::Rails::Controller
+  before_filter :prepare_for_mobile
+
+
+  private
+
+  def prepare_for_mobile
+    if request.env['mobvious.device_type'] == :mobile
+      request.format = :mobile
+    end
+  end
 end
