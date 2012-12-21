314f81f2ed40768cf085e7fa5ab0a87e69fdc27b
Chris Maxwell <cmaxwell@ojala.com>
2012-12-21 17:22:23 -0500

    Replace navigation with icon-fonts.

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,9 @@ module Viewthought
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    # config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
     # Tell your app to use Mobvious::Manager as Rack middleware.
     config.middleware.use Mobvious::Manager
 
