d383b23e0513a86a487dab6728a6c34903fa0625
Chris Maxwell <cmaxwell@ojala.com>
2013-01-08 15:26:27 -0500

    According to the guides 'You should put the root route at the top of the file, because it is the most popular route and should be matched first.'

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Viewthought::Application.routes.draw do
+  root :to => 'pages#home'
+
   match 'home'      => 'pages#home'
   match 'work'      => 'pages#work'
   match 'team'      => 'pages#team'
@@ -52,10 +54,6 @@ Viewthought::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
