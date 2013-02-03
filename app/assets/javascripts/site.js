46d8e679ef963e9f1e76f69ab062716448d1a1b6
Chris Maxwell <cmaxwell@ojala.com>
2013-02-03 16:38:47 -0500

    Update layout to use Susy, test in navigation element.

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,7 +1,7 @@
 // Lettering.js
 
 $(document).ready(function() {
-  $(".viewthought").lettering();
+  $(".logo").lettering();
 });
 
 
@@ -10,7 +10,7 @@ $(document).ready(function() {
 $(function() {
   // As soon as the DOM is ready, make invisible
   $(document).ready(function() {
-    $('.viewthought').css('visibility', 'hidden');
+    $('.logo').css('visibility', 'hidden');
   });
 
   // Load the fonts
@@ -20,15 +20,15 @@ $(function() {
         // As soon as the fonts are active, fade in
         // Don't fade in browsers that don't do proper opacity, like IE
         if (jQuery.support.opacity) {
-          $('.viewthought').css('visibility', 'visible').hide().fadeIn();
+          $('.logo').css('visibility', 'visible').hide().fadeIn();
         } else {
-          $('.viewthought').css('visibility', 'visible');
+          $('.logo').css('visibility', 'visible');
         }
       },
       inactive: function() {
         // If the fonts are inactive, just show
         // You can apply fallback styles using the wf-inactive class in your CSS
-        $('.viewthought').css('visibility', 'visible');
+        $('.logo').css('visibility', 'visible');
       }
     })
   } catch(e) {}
