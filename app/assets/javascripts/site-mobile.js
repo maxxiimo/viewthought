5e9c6cf920ec27dd6eb99390696e3aa8318967d3
Chris Maxwell <cmaxwell@ojala.com>
2013-07-03 12:53:05 -0400

    Separate mobile javascript into own distinct files. Magic line script not necessary in mobile and causes errors in subsequent scripts.

---
+++ app/assets/javascripts/site-mobile.js
@@ -0,0 +1,35 @@
+// Lettering.js
+
+$(document).ready(function() {
+  $(".logo").lettering();
+});
+
+
+// Typekit
+
+$(function() {
+  // As soon as the DOM is ready, make invisible
+  $(document).ready(function() {
+    $('.logo').css('visibility', 'hidden');
+  });
+
+  // Load the fonts
+  try {
+    Typekit.load({
+      active: function() {
+        // As soon as the fonts are active, fade in
+        // Don't fade in browsers that don't do proper opacity, like IE
+        if (jQuery.support.opacity) {
+          $('.logo').css('visibility', 'visible').hide().fadeIn();
+        } else {
+          $('.logo').css('visibility', 'visible');
+        }
+      },
+      inactive: function() {
+        // If the fonts are inactive, just show
+        // You can apply fallback styles using the wf-inactive class in your CSS
+        $('.logo').css('visibility', 'visible');
+      }
+    })
+  } catch(e) {}
+})();
