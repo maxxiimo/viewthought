7b2aeaa2d773345789c9bf15ee4cb233bc326327
Chris Maxwell <cmaxwell@ojala.com>
2013-07-30 02:12:37 -0400

    Reinstate Anystretch and original hero picture.

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -8,6 +8,13 @@ $(document).ready(function() {
 
 
 // ===========================
+// Anystretch
+// ===========================
+
+$('#hero-pic').anystretch("assets/pics/silhouette-1024-maximum.jpg", {speed: 200, positionY: 'top'});
+
+
+// ===========================
 // bgStretcher
 // ===========================
 
