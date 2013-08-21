26b79e3d599b9454f06cc465a305423edb89640f
Chris Maxwell <cmaxwell@ojala.com>
2013-08-21 10:57:54 -0400

    Change asset host.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Viewthought::Application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
   # config.action_controller.asset_host = "http://dt1m8zdqb95uu.cloudfront.net"
-  config.action_controller.asset_host = "http://assets%d.viewthought.com"
+  config.action_controller.asset_host = "http://cdn%d.viewthought.com"
 
   # To set the origin domain that will get set in the Access-Control-Allow-Origin header.
   # https://github.com/rubymaverick/font_assets#usage
