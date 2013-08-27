dfe01d33fda7fb2c58e985b4416f341bc51abb81
Chris Maxwell <cmaxwell@ojala.com>
2013-08-27 16:46:24 -0400

    Remove scroll position script from mobile, adjust tagline breaks.

--- app/assets/javascripts/site_mobile.js
+++ app/assets/javascripts/site_mobile.js
@@ -1,6 +1,5 @@
 // Lettering
 // Anystretch
-// Adjust Arrow Location
 // Remove Anchor Outline
 // Smooth Scroll
 
@@ -20,42 +19,6 @@ $(document).ready(function() {
 
 $('#photo-hero').anystretch("assets/pics/silhouette-320-maximum.jpg", {speed: 200, positionY: 'top'});
 $('#in-person').anystretch("assets/smush_map-320.png", {speed: 200, positionY: 'top'});
-
-
-// ===========================
-// Adjust Arrow Location
-// ===========================
-
-$(document).ready(function () {
-    var windowHeight, headHeight, heroHeight, expertiseHeight;
-
-    function init() {
-        windowHeight = $(window).height();
-        headHeight = $('#home').height();
-        arrowHeight = $('#photo-hero .scroll').height();
-        heroHeight = $('#photo-hero').outerHeight();
-        expertiseHeight = $('#expertise').outerHeight();
-
-        // FIXME ccm: Why doesn't this work properly on resize?
-        // $('#photo-hero .scroll').css('top', windowHeight - (arrowHeight + headHeight));
-        // $('#expertise .scroll').css('top', windowHeight - (arrowHeight - 10))
-
-        if (heroHeight + headHeight > windowHeight) {
-            $('#photo-hero .scroll').css('top', windowHeight - 110);
-        }
-        if (expertiseHeight > windowHeight) {
-            $('#about-us .scroll').css('top', windowHeight - 60)
-        }
-        if (expertiseHeight > windowHeight) {
-            $('#expertise .scroll').css('top', windowHeight - 60)
-        };
-    }
-    init();
-    $(window).resize(function () {
-
-        init();
-    });
-});
 
 
 // ===========================
