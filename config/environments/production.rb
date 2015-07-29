ed946a15d0eeb2fbae3ca282c42b1e9fa56ce079
Chris Maxwell <cmaxwell@ojala.com>
2015-07-29 12:18:46 -0700

    Add cloudfront URL, cdn0.ojala-inc.com not yet set.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,8 +57,8 @@ Viewthought::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
-  # config.action_controller.asset_host = "http://dt1m8zdqb95uu.cloudfront.net"
-  config.action_controller.asset_host = "http://cdn%d.ojalainc.com"
+  config.action_controller.asset_host = "http://d35c9j2nrzldis.cloudfront.net"
+  # config.action_controller.asset_host = "http://cdn%d.ojalainc.com"
 
   # To set the origin domain that will get set in the Access-Control-Allow-Origin header.
   # https://github.com/rubymaverick/font_assets#usage
