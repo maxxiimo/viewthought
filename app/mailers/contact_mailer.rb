6ac7985054da84377b3078597c10bf94a043b1e1
Chris Maxwell <cmaxwell@ojala.com>
2015-07-29 17:12:54 -0700

    Change ViewThought to Ojala, Inc.

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,9 @@
 class ContactMailer < ActionMailer::Base
-  default from: "no-reply@viewthought.com"
+  default from: "no-reply@ojalainc.com"
 
   def contact_us_message(message)
     @message = message
 
-    mail to: "cmaxwell@ojala.com", subject: "Contact Us Form Submission"
+    mail to: "chris.maxwell@ojalainc.com", subject: "Contact Us Form Submission"
   end
 end
